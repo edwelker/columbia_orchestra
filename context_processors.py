adc48f4e211baeb05686c86d1cce077855fd4a13
Edward Welker <eddie.welker@gmail.com>
2010-04-11 03:04:16 -0400

    got Twitter updates fixed, and added regex for url escaping. updated ignores to exclude py~ files, and removed pyc files from the repo

--- context_processors.py
+++ context_processors.py
@@ -2,18 +2,24 @@ from datetime import datetime
 from django.conf import settings
 from django.core.cache import cache
 import twitter
+import re
+
 
 def latest_tweets( request ):
     tweets = cache.get( 'tweets' )
-	
+    
     if tweets:
         return {"tweets": tweets}
 
     dates = []
+    pattern = r'''\b((?:(?:http)://|www\.)[-a-zA-Z0-9+&@#/%=~_|$?!:,.]*[a-zA-Z0-9+&@#/%=~_|$])'''
+    replacement = '<a href="\\1">\\1</a>'
 
-    tweets = twitter.Api(username='31street', password='31molor').GetUserTimeline( settings.TWITTER_USER )[0:7]
+    api = twitter.Api()
+    tweets = api.GetUserTimeline( settings.TWITTER_USER, count=2 )
     for t in tweets:
         t.date = datetime.strptime( t.created_at, "%a %b %d %H:%M:%S +0000 %Y" )
+        t.text = re.sub(pattern, replacement, t.text)
     cache.set( 'tweets', tweets, settings.TWITTER_TIMEOUT )
 
     return {"tweets": tweets, "dates": dates}
--- homepage/views.py
+++ homepage/views.py
@@ -8,6 +8,8 @@ import datetime
 
 def home(request):
     m = OrchestraMember.objects.all()[0] #will be changed be randomized later
-    e = Event.objects.filter(date__gte=datetime.datetime.today()).order_by("date")[0]
+    e = Event.objects.filter(date__gte=datetime.datetime.today()).order_by("date")
+        
+    event = e[0] if (len(e) > 0) else None
     
-    return render_to_response('home.html', {'member': m, 'event': e}, context_instance=RequestContext(request))
+    return render_to_response('home.html', {'member': m, 'event': event}, context_instance=RequestContext(request))
