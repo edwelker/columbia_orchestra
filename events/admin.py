dbef7361f113573fdd81dc80a6c6962d6cc9a36b
Edward Welker <eddie.welker@gmail.com>
2010-04-19 16:36:06 -0400

    small changes to admin, have to quit for the day

--- events/admin.py
+++ events/admin.py
@@ -2,30 +2,33 @@ from orch.events.models import Season, Event, Location, Soloist, PreConcertDiscu
 from django.contrib import admin
 
 class SeasonAdmin(admin.ModelAdmin):
-	prepopulated_fields = {'slug': ['name']}
-	list_display = ('name', 'startdate', 'enddate')
+    prepopulated_fields = {'slug': ['name']}
+    list_display = ('name', 'startdate', 'enddate', 'description')
 
 admin.site.register(Season, SeasonAdmin)
 
 class LocationAdmin(admin.ModelAdmin):
-	prepopulated_fields = {'slug': ['name']}
-	list_display = ('name', 'city', 'zip')
-	
+    prepopulated_fields = {'slug': ['name']}
+    list_display = ('name', 'city', 'zip')
+    
 admin.site.register(Location, LocationAdmin)
 
 class EventAdmin(admin.ModelAdmin):
-	prepopulated_fields = {'slug': ['name']}
+    prepopulated_fields = {'slug': ['name']}
+    list_display = ('name', 'date', 'location', 'season')
+    list_filter = ('location',)
+    
 
 admin.site.register(Event, EventAdmin)
 
 class PreConcertDiscussionAdmin(admin.ModelAdmin):
-	pass
+    pass
 
 admin.site.register(PreConcertDiscussion)
 
 class SoloistAdmin(admin.ModelAdmin):
-	prepopulated_fields = {'slug': ['name']}
-	list_display = ('name', 'instrument', 'work')
-	list_filter = 'instrument',
+    prepopulated_fields = {'slug': ['name']}
+    list_display = ('name', 'instrument', 'work')
+    list_filter = ('instrument',)
 
 admin.site.register(Soloist, SoloistAdmin)
