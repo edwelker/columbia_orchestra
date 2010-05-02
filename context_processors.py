c58a7020c8fc938272b0239e2beb959ae5f352ad
Edward Welker <eddie.welker@gmail.com>
2010-05-02 12:43:21 -0400

    facebook cache added

--- context_processors.py
+++ context_processors.py
@@ -6,6 +6,8 @@ import re
 from dateutil.parser import parse
 from django.http import HttpResponseServerError
 
+pattern = r'''\b((?:(?:http)://|www\.)[-a-zA-Z0-9+&@#/%=~_|$?!:,.]*[a-zA-Z0-9+&@#/%=~_|$])'''
+replacement = '<a href="\\1">\\1</a>'
 
 def latest_tweets( request ):
     tweets = cache.get( 'tweets' )
@@ -14,8 +16,6 @@ def latest_tweets( request ):
         return {"tweets": tweets}
 
     dates = []
-    pattern = r'''\b((?:(?:http)://|www\.)[-a-zA-Z0-9+&@#/%=~_|$?!:,.]*[a-zA-Z0-9+&@#/%=~_|$])'''
-    replacement = '<a href="\\1">\\1</a>'
 
     api = twitter.Api()
     try:
@@ -31,13 +31,21 @@ def latest_tweets( request ):
 
 
 def latest_facebook( request ):
+    fb = cache.get( 'fb' )
     
-    feed = feedparser.parse('http://www.facebook.com/feeds/page.php?format=atom10&id=211864312316')
+    if fb: 
+        return {'fb': fb}
+
+    try:
+        feed = feedparser.parse('http://www.facebook.com/feeds/page.php?format=atom10&id=211864312316')    
+        fb = feed['entries'][0:2]
+        #dates = [parse(d.published) for d in fb]
     
-    fb = feed['entries'][0:1]
-    #dates = [parse(d.published) for d in fb]
-    
-    for entry in fb:
-        entry['fixed_date'] = parse(entry.published)
+        for entry in fb:
+            entry['fixed_date'] = parse(entry.published)
+            
+        cache.set( 'fb', fb, settings.TWITTER_TIMEOUT )    
+    except:
+        return HttpResponseServerError('Facebook failure')
     
     return { 'fb': fb }
