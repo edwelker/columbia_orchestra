51e3fc470e8f7ca651769af5404373633635f712
Edward Welker <eddie.welker@gmail.com>
2010-05-02 20:58:37 -0400

    odds and ends

--- roster/views.py
+++ roster/views.py
@@ -5,7 +5,7 @@ from orch.roster.models import OrchestraMember
 # Create your views here.
 def all_members(request):
     m = OrchestraMember.objects.exclude(noncurrent_member=True).order_by('instrument', '-principal', 'last_name')
-    return render_to_response('members.html', {'members': m, 'cut': cut})
+    return render_to_response('members.html', {'members': m})
 
 def member(request, first_name, last_name):
     m = get_object_or_404(OrchestraMember, first_name__iexact=first_name, last_name__iexact=last_name)
